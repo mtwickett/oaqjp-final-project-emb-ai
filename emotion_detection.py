16ef8df40fab49636dcd59f3d8d7cfaa761e7bb6
Martin Wickett <martintwickett@gmail.com>
2026-01-28 20:17:12 -0500

    Extracting json logic

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,11 +1,22 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    
     response = requests.post(
         url,
         json={ "raw_document": { "text": text_to_analyze } },
         headers=headers
         )
-    return response.text
+    
+    json_response = response.json()
+    
+    emotion_scores = json_response.get('emotionPredictions',[{}])[0].get('emotion',{})
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    
+    return {**emotion_scores, "dominant_emotion": dominant_emotion}
+
+
+print(emotion_detector('I am so happy I am doing this.'))
